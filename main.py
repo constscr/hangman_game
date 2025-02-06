cf4090ef194035284294ca7f60f37bdb67438455
Konstantin Golubev <konstantin-golubev-2002@mail.ru>
2025-02-07 01:49:32 +0300

    Fix code based on PR comments

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from src.hangman_game.hangman import HangmanGame
+from src.hangman_game.hangman import (
+    HangmanGame,
+    InvalidLetterError,
+    DuplicateLetterError
+)
+
 
 def main():
     game = HangmanGame(max_attempts=7)
 
     try:
         game.load_words("data/words_stock_rus.txt")
-    except Exception as e:
-        print(f"Error loading words: {e}")
-        return
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    except IOError as e:
+        print(f"Error: {e}")
 
     while True:
         game.generate_word()
@@ -17,9 +23,11 @@ def main():
         while not game.is_game_over():
             game.display_game_status()
 
-            guess = input("Enter a letter: ").strip()
-
-            game.guess_letter(guess)
+            try:
+                guess = input("Enter a letter: ").strip()
+                game.guess_letter(guess)
+            except (InvalidLetterError, DuplicateLetterError) as e:
+                print(f"\nError guessing: {e}")
 
         game.display_game_result()
 
--- src/hangman_game/hangman.py
+++ src/hangman_game/hangman.py
@@ -1,81 +1,99 @@
 from random import choice
+from enum import Enum
+from typing import List, Set, Optional
+
+
+class InvalidLetterError(Exception):
+    """Exception for an invalid letter."""
+    pass
+
+class DuplicateLetterError(Exception):
+    """Exception for an already guessed letter."""
+    pass
+
+class GameStatus(Enum):
+    in_progress = 1
+    won = 2
+    lost = 3
 
 class HangmanGame:
     """Hangman's Game."""
 
-    def __init__(self, max_attempts=7):
-        self.max_attempts = max_attempts
-        self.words = []
-        self.secret_word = ''
-        self.guessed_letters = set()
-        self.attempts = 0
+    def __init__(self, max_attempts: int = 7) -> None:
+        self.max_attempts: int = max_attempts
+        self.words: List[str] = []
+        self.secret_word: str = ''
+        self.guessed_letters: Set[str] = set()
+        self._attempts: int = 0
+        self.current_state: str = ""
+        self.status: GameStatus = GameStatus.in_progress
 
-    def load_words(self, filename):
+    def load_words(self, filename: str) -> None:
         """Loads the word list from a UTF-8 encoded file."""
         try:
             with open(filename, 'r', encoding='utf-8') as file:
-                self.words = [word.strip().lower() for word in  file.readlines()]
+                self.words = [word.strip().lower() for word in file.readlines()]
         except FileNotFoundError:
-            print(f"File {filename} not found.")
-            raise
+            raise FileNotFoundError(f"File {filename} not found.")
 
-    def generate_word(self):
+    def generate_word(self) -> None:
         """Generates a random word from the list."""
         self.secret_word = choice(self.words)
-        self.attempts = self.max_attempts
+        self._attempts = self.max_attempts
         self.guessed_letters.clear()
+        self.current_state = '_' * len(self.secret_word)
+        self._update_status()
 
-    def guess_letter(self, letter):
+    def guess_letter(self, letter: str) -> None:
         """Processes the player's guess (passing the letter)."""
         letter = letter.lower()
 
         if len(letter) != 1 or not letter.isalpha():
-            print(f"\nLetter {letter} is invalid. Try again.")
-            return
+            raise InvalidLetterError(f"\nLetter {letter} is invalid. Try again.")
 
         if letter in self.guessed_letters:
-            print(f"\nYou've already tried the letter {letter}. Try another letter.")
-            return
+            raise DuplicateLetterError(f"\nYou've already tried the letter {letter}. Try another letter.")
 
         self.guessed_letters.add(letter)
 
+        self.current_state = ''.join([char if char in self.guessed_letters
+                                      else '_' for char in self.secret_word])
+
         if letter not in self.secret_word:
-            self.attempts -= 1
+            self._attempts -= 1
 
-    def get_current_state(self):
+        self._update_status()
+
+    def _update_status(self) -> None:
+        """Updates game status based on current state."""
+        if self.secret_word == self.get_current_state():
+            self.status = GameStatus.won
+        elif self._attempts <= 0:
+            self.status = GameStatus.lost
+        else:
+            self.status = GameStatus.in_progress
+
+    def is_game_over(self) -> bool:
+        """Checks if the game is over."""
+        return self.status != GameStatus.in_progress
+
+    def get_current_state(self) -> str:
         """Returns the current state of the word (with open and hidden letters)."""
-        return ''.join([char if char in self.guessed_letters
-                         else '_' for char in self.secret_word])
+        return self.current_state
 
-    def get_used_letters(self):
+    def get_used_letters(self) -> List[str]:
         """Returns a sorted list of letters used."""
         return sorted(self.guessed_letters)
 
-    def display_game_status(self):
+    def display_game_status(self) -> None:
         """Displays the current status of the game."""
         print(f"\nThe hidden word: {self.get_current_state()}")
         print(f"The letters used are: {', '.join(self.get_used_letters())}")
-        print(f"Attempts remaining: {self.attempts}")
+        print(f"Attempts remaining: {self._attempts}")
 
-    def check_win(self):
-        """Checks if the conditions for winning are met."""
-        return self.secret_word == self.get_current_state()
-
-    def check_lose(self):
-        """Checks whether the conditions for defeat are met."""
-        return self.attempts <= 0
-
-    def is_game_over(self):
-        """Checks if the game is over."""
-        if self.check_win():
-            return True
-        elif self.check_lose():
-            return True
-        return False
-
-    def display_game_result(self):
+    def display_game_result(self) -> None:
         """Displays the result of the game."""
-        if self.check_win():
+        if self.status == GameStatus.won:
             print(f"\nCongratulations! You've won! Word: {self.secret_word}")
-        elif self.check_lose():
+        elif self.status == GameStatus.lost:
             print(f"\nYou've been hanged! The hidden word: {self.secret_word}")
